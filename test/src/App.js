46f6a7e9a7b20e42cb12ffadd492088acbe47b08
JohnJohnMan <jjm4248@gmail.com>
2024-05-29 14:50:24 +0900

    README

--- test/src/App.js
+++ test/src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './Welcome';
+import CommentList from './CommentList';
+import Counter from './Counter';
+import MeasureEx from './MeasureEx';
+import Accommodate from './Accommodate';
+import Toggle from './Amrk';
+import MyButton from './MyButton';
+import LoginControl from './LoginControl';
+import MainPage from './MainPage';
+import LandingPage from './LandingPage';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <LandingPage />
+    </>
+    /*<div className="App">
+      <CommentList />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello !!!Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,11 +29,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React gogo
+          Learn React
         </a>
       </header>
-    </div>
+      <Welcome name = "전준만"/> 
+    </div> */
   );
 }
 
 export default App;
+
+//npx create-react-app name
--- test/src/index.js
+++ test/src/index.js
@@ -10,7 +10,7 @@ import Hello from './Hello';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello />
+    <App />
     </React.StrictMode>
 );
 // )
